e9e4eac6d2e4250f1e6b0eab9f2922ee647452fb
f122apg <f122apg@gmail.com>
2016-03-30 00:47:59 +0900

    First commit

---
+++ mp3tageditor/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace mp3tageditor
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if(disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.listView1 = new System.Windows.Forms.ListView();
+			this.Filename_columnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.Path_columnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.Songname_columnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.Songtime_columnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.Artist_textbox = new System.Windows.Forms.TextBox();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.Datasize_label = new System.Windows.Forms.Label();
+			this.Imagesize_label = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// listView1
+			// 
+			this.listView1.AllowDrop = true;
+			this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Filename_columnHeader,
+            this.Path_columnHeader,
+            this.Songname_columnHeader,
+            this.Songtime_columnHeader});
+			this.listView1.FullRowSelect = true;
+			this.listView1.GridLines = true;
+			this.listView1.Location = new System.Drawing.Point(12, 12);
+			this.listView1.MultiSelect = false;
+			this.listView1.Name = "listView1";
+			this.listView1.Size = new System.Drawing.Size(706, 363);
+			this.listView1.TabIndex = 0;
+			this.listView1.UseCompatibleStateImageBehavior = false;
+			this.listView1.View = System.Windows.Forms.View.Details;
+			this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+			this.listView1.DragDrop += new System.Windows.Forms.DragEventHandler(this.listView1_DragDrop);
+			this.listView1.DragEnter += new System.Windows.Forms.DragEventHandler(this.listView1_DragEnter);
+			// 
+			// Filename_columnHeader
+			// 
+			this.Filename_columnHeader.Text = "ファイル名";
+			this.Filename_columnHeader.Width = 151;
+			// 
+			// Path_columnHeader
+			// 
+			this.Path_columnHeader.Text = "パス";
+			this.Path_columnHeader.Width = 288;
+			// 
+			// Songname_columnHeader
+			// 
+			this.Songname_columnHeader.Text = "曲名";
+			this.Songname_columnHeader.Width = 178;
+			// 
+			// Songtime_columnHeader
+			// 
+			this.Songtime_columnHeader.Text = "曲の長さ";
+			this.Songtime_columnHeader.Width = 70;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(640, 406);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 384);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(63, 12);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "アーティスト：";
+			// 
+			// Artist_textbox
+			// 
+			this.Artist_textbox.Location = new System.Drawing.Point(81, 381);
+			this.Artist_textbox.Name = "Artist_textbox";
+			this.Artist_textbox.ReadOnly = true;
+			this.Artist_textbox.Size = new System.Drawing.Size(637, 19);
+			this.Artist_textbox.TabIndex = 3;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(81, 406);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(160, 153);
+			this.pictureBox1.TabIndex = 4;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 406);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(65, 12);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "アートワーク：";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(247, 528);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(68, 12);
+			this.label3.TabIndex = 6;
+			this.label3.Text = "データサイズ：";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(247, 547);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(40, 12);
+			this.label4.TabIndex = 7;
+			this.label4.Text = "大きさ：";
+			// 
+			// Datasize_label
+			// 
+			this.Datasize_label.AutoSize = true;
+			this.Datasize_label.Location = new System.Drawing.Point(321, 528);
+			this.Datasize_label.Name = "Datasize_label";
+			this.Datasize_label.Size = new System.Drawing.Size(27, 12);
+			this.Datasize_label.TabIndex = 8;
+			this.Datasize_label.Text = "NaN";
+			// 
+			// Imagesize_label
+			// 
+			this.Imagesize_label.AutoSize = true;
+			this.Imagesize_label.Location = new System.Drawing.Point(293, 547);
+			this.Imagesize_label.Name = "Imagesize_label";
+			this.Imagesize_label.Size = new System.Drawing.Size(27, 12);
+			this.Imagesize_label.TabIndex = 9;
+			this.Imagesize_label.Text = "NaN";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(727, 571);
+			this.Controls.Add(this.Imagesize_label);
+			this.Controls.Add(this.Datasize_label);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.Artist_textbox);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.listView1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ListView listView1;
+		private System.Windows.Forms.ColumnHeader Filename_columnHeader;
+		private System.Windows.Forms.ColumnHeader Path_columnHeader;
+		private System.Windows.Forms.ColumnHeader Songname_columnHeader;
+		private System.Windows.Forms.ColumnHeader Songtime_columnHeader;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox Artist_textbox;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label Datasize_label;
+		private System.Windows.Forms.Label Imagesize_label;
+	}
+}
+
---
+++ mp3tageditor/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace mp3tageditor
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void listView1_DragEnter(object sender, DragEventArgs e)
+		{
+			string[] DragFiles = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+			//mp3ファイル以外のファイルがドロップされてないか確認する
+			for(int i = 0; i < DragFiles.Length; i++)
+			{
+				//ドラックされたファイルに.mp3がついていなければ、mp3ファイル以外のファイルがドラックされているとして
+				//e.EffectをNoneに設定し、break
+				if(DragFiles[i].IndexOf(".mp3", DragFiles[i].Length - 4) != -1)
+				{
+					if(i == DragFiles.Length - 1)
+						e.Effect = DragDropEffects.Copy;
+				}
+				else
+				{
+					e.Effect = DragDropEffects.None;
+					break;
+				}
+			}
+		}
+
+		private void listView1_DragDrop(object sender, DragEventArgs e)
+		{
+			string[] Mp3Paths = (string[])e.Data.GetData(DataFormats.FileDrop);
+			
+			for(int i = 0; i < Mp3Paths.Length; i ++)
+			{
+				//Mp3ファイルを開く
+				using(TagLib.File mp3 = TagLib.File.Create(Mp3Paths[i]))
+				{
+					//Mp3ファイルのTagを取得
+					TagLib.Tag mp3tags = mp3.Tag;
+					//Mp3ファイルのプロパティを取得
+					TagLib.Properties mp3prop = mp3.Properties;
+
+					//listviewに追加するアイテムを用意する
+					//ファイル名|パス|タグから読み込んだ曲名|曲の長さ
+					string[] items =
+						{ Mp3Paths[i].Substring(Mp3Paths[i].LastIndexOf('\\') + 1),
+							Mp3Paths[i],
+							mp3tags.Title,
+							new DateTime(0).Add(mp3prop.Duration).ToString("HH:mm:ss")
+						};
+					//listviewにアイテムを追加
+					listView1.Items.Add(new ListViewItem(items));
+				}
+			}
+		}
+
+		private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			if(listView1.SelectedItems.Count > 0)
+			{
+				//listviewで選択されているmp3ファイルを開く
+				using(TagLib.File mp3 = TagLib.File.Create(listView1.SelectedItems[0].SubItems[1].Text))
+				{
+					TagLib.Tag mp3tags = mp3.Tag;
+
+					foreach(string artist in mp3tags.Performers)
+					{
+						//アーティストを取得
+						Artist_textbox.Text = artist;
+					}
+
+					MemoryStream ms;
+					try
+					{
+						//アートワークを取得
+						TagLib.IPicture Artworks = mp3tags.Pictures[0];
+						ms = new MemoryStream(Artworks.Data.Data);
+						ms.Seek(0, SeekOrigin.Begin);
+
+						//画像をリサイズ
+						Bitmap canvas = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+						Graphics g = Graphics.FromImage(canvas);
+						g.DrawImage(Image.FromStream(ms), 0, 0, pictureBox1.Width, pictureBox1.Height);
+						g.Dispose();
+
+						pictureBox1.Image = canvas;//ImageResize(Image.FromStream(ms), 400, 400);
+
+						Image backupimg;
+						using(Image imgsrc = Image.FromStream(ms))
+							backupimg = new Bitmap(imgsrc);
+						using(backupimg)
+						{
+							//picturebox1をクリックした時の画像表示のために一時的にアートワークを保存
+							backupimg.Save(TempImageFilePath, ImageFormat.Png);
+						}
+					}
+					//アートワークが設定されていない場合
+					catch(IndexOutOfRangeException)
+					{
+						pictureBox1.Image = null;
+					}
+				}
+			}
+		}
+
+		private void pictureBox1_Click(object sender, EventArgs e)
+		{
+			ImageShow(null, Image.FromFile(TempImageFilePath));
+		}
+
+		/// <summary>
+		/// 引数urlから画像をダウンロードし、新しいフォームに表示する。
+		/// どちらかの引数は必ず指定しなければならない。どちらも指定した場合、第一引数を優先して表示する。
+		/// </summary>
+		/// <param name="url">画像が格納されたURLを指定。省略可。string型</param>
+		/// <param name="img">画像が格納されたImageを指定。省略可。Image型</param>
+		private void ImageShow(string url = null, Image img = null)
+		{
+			Image imgdata = null;
+
+			if(url != null)
+			{
+				HttpWebRequest hwreq = (HttpWebRequest)WebRequest.Create(new Uri(url));
+				using(HttpWebResponse hwres = (HttpWebResponse)hwreq.GetResponse())
+				{
+					//画像をダウンロード
+					Stream imgstream = hwres.GetResponseStream();
+					imgdata = Image.FromStream(imgstream);
+				}
+			}
+			else
+				imgdata = img;
+
+			//フォーム作成
+			Form imageshow = new Form();
+			imageshow.StartPosition = FormStartPosition.Manual;
+			imageshow.Location = new Point(this.Left + this.Width, this.Top);
+			Size windowsize = new Size(400, 400);
+			//画像表示のためのpictureboxを作成
+			PictureBox pb = new PictureBox();
+			Bitmap canvas = new Bitmap(windowsize.Width, windowsize.Height);
+			////リサイズするサイズを設定
+			//Rectangle destrect = new Rectangle(0, 0, windowsize.Width, windowsize.Height);
+			////解像度をオリジナルの画像と同等に設定
+			//canvas.SetResolution(imgdata.HorizontalResolution, imgdata.VerticalResolution);
+			using(Graphics g = Graphics.FromImage(canvas))
+			{
+				////高画質化
+				//g.CompositingMode = CompositingMode.SourceCopy;
+				//g.CompositingQuality = CompositingQuality.HighQuality;
+				//g.InterpolationMode = InterpolationMode.Bicubic;
+				//g.SmoothingMode = SmoothingMode.HighQuality;
+				//g.PixelOffsetMode = PixelOffsetMode.HighQuality;x
+
+				//using(ImageAttributes wrapmode = new ImageAttributes())
+				//{
+				//	wrapmode.SetWrapMode(WrapMode.TileFlipXY);
+				//	g.DrawImage(imgdata, destrect, 0, 0, windowsize.Width, windowsize.Height, GraphicsUnit.Pixel, wrapmode);
+				//}
+				g.DrawImage(imgdata, 0, 0, windowsize.Width, windowsize.Height);
+			}
+			
+			//先ほどダウンロードした画像をセット or ロードした画像をセット
+			pb.Image = canvas;
+			pb.SizeMode = PictureBoxSizeMode.AutoSize;
+			
+			imageshow.Size = pb.Size + new Size(0, 28);
+			//pictureboxコントロールをフォームに追加
+			imageshow.Controls.Add(pb);
+			imageshow.Show(this);
+		}
+
+		private Image ImageResize(Image img, int width, int height)
+		{
+			Console.WriteLine(width + " " + height);
+			Bitmap canvas = new Bitmap(width, height);
+			Graphics g = Graphics.FromImage(canvas);
+			g.DrawImage(img, 0, 0, width, height);
+			g.Dispose();
+
+			return canvas;
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			ImageShow("https://pbs.twimg.com/profile_images/585714416643088384/oNc3OwuT.png");
+		}
+
+		private static string tempimagefilepath = Path.GetTempFileName();
+		public static string TempImageFilePath
+		{
+			get { return tempimagefilepath; }
+		}
+
+	}
+}
