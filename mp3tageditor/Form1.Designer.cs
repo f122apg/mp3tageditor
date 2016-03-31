0649a62e5ee984da51f87984f48aaa044b451df7
f122apg <f122apg@gmail.com>
2016-03-31 21:01:43 +0900

    2016-3-31 変更箇所など [Added] 1.データサイズとアートワークのサイズを見られるようにした
    [changed]
    1.関数ImageShowの画像をダウンロードするクラスを変更し、簡略化を図った
     before:httpwebrequest -> after:httpclient
    2.関数ImageShowの画像をダウンロード処理を非同期化した
    
    [bugfixed]
    1.tempfileを使いまわしで保存していた処理にバグが発生していたが
      使い回しせずに曲ごとにファイルを別々に保存するようにして、バグを修正した
    2.アートワークが設定されていない曲を追加するとpictureboxの画像表示がおかしくなるバグを修正

--- mp3tageditor/Form1.Designer.cs
+++ mp3tageditor/Form1.Designer.cs
@@ -143,7 +143,7 @@
 			// label4
 			// 
 			this.label4.AutoSize = true;
-			this.label4.Location = new System.Drawing.Point(247, 547);
+			this.label4.Location = new System.Drawing.Point(275, 547);
 			this.label4.Name = "label4";
 			this.label4.Size = new System.Drawing.Size(40, 12);
 			this.label4.TabIndex = 7;
@@ -152,20 +152,20 @@
 			// Datasize_label
 			// 
 			this.Datasize_label.AutoSize = true;
-			this.Datasize_label.Location = new System.Drawing.Point(321, 528);
+			this.Datasize_label.Location = new System.Drawing.Point(312, 528);
 			this.Datasize_label.Name = "Datasize_label";
-			this.Datasize_label.Size = new System.Drawing.Size(27, 12);
+			this.Datasize_label.Size = new System.Drawing.Size(21, 12);
 			this.Datasize_label.TabIndex = 8;
-			this.Datasize_label.Text = "NaN";
+			this.Datasize_label.Text = "NA";
 			// 
 			// Imagesize_label
 			// 
 			this.Imagesize_label.AutoSize = true;
-			this.Imagesize_label.Location = new System.Drawing.Point(293, 547);
+			this.Imagesize_label.Location = new System.Drawing.Point(312, 547);
 			this.Imagesize_label.Name = "Imagesize_label";
-			this.Imagesize_label.Size = new System.Drawing.Size(27, 12);
+			this.Imagesize_label.Size = new System.Drawing.Size(21, 12);
 			this.Imagesize_label.TabIndex = 9;
-			this.Imagesize_label.Text = "NaN";
+			this.Imagesize_label.Text = "NA";
 			// 
 			// Form1
 			// 
--- mp3tageditor/Form1.cs
+++ mp3tageditor/Form1.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Drawing;
-using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.Windows.Forms;
-using System.Net;
+using System.Net.Http;
 using System.IO;
+using System.Collections.Generic;
 
 namespace mp3tageditor
 {
@@ -14,6 +14,7 @@ namespace mp3tageditor
 		{
 			InitializeComponent();
 		}
+		private static int counter = 0;
 
 		private void listView1_DragEnter(object sender, DragEventArgs e)
 		{
@@ -39,7 +40,7 @@ namespace mp3tageditor
 		private void listView1_DragDrop(object sender, DragEventArgs e)
 		{
 			string[] Mp3Paths = (string[])e.Data.GetData(DataFormats.FileDrop);
-			
+
 			for(int i = 0; i < Mp3Paths.Length; i ++)
 			{
 				//Mp3ファイルを開く
@@ -50,7 +51,7 @@ namespace mp3tageditor
 					//Mp3ファイルのプロパティを取得
 					TagLib.Properties mp3prop = mp3.Properties;
 
-					//listviewに追加するアイテムを用意する
+					//listviewに追加するアイテムを準備する
 					//上から ファイル名|パス|タグから読み込んだ曲名|曲の長さ
 					string[] items =
 						{ Mp3Paths[i].Substring(Mp3Paths[i].LastIndexOf('\\') + 1),
@@ -58,8 +59,36 @@ namespace mp3tageditor
 							mp3tags.Title,
 							new DateTime(0).Add(mp3prop.Duration).ToString("HH:mm:ss")
 						};
+					Console.WriteLine(Mp3Paths[i]);
+
 					//listviewにアイテムを追加
 					listView1.Items.Add(new ListViewItem(items));
+
+					//ドラッグアンドドロップされた曲のアートワークを一括で取得
+					MemoryStream ms;
+					try
+					{
+						//アートワークの取得
+						TagLib.IPicture Artworks = mp3tags.Pictures[0];
+						ms = new MemoryStream(Artworks.Data.Data);
+						ms.Seek(0, SeekOrigin.Begin);
+						Image artworkimg = Image.FromStream(ms);
+
+						Guid guid = Guid.NewGuid();
+						//pictureboxに表示するために一時的に画像ファイルとして保存する
+						artworkimg.Save("tmp" + guid.ToString() + ".png", ImageFormat.Png);
+						TempImageFilePaths.Add(new List<string> { listView1.Items[counter].SubItems[1].Text,
+							"tmp" + guid.ToString() + ".png" });
+						//ファイルに属性hiddinを追加
+						if(TempImageFilePaths[counter][1] != null)
+							File.SetAttributes(TempImageFilePaths[counter][1], FileAttributes.Hidden);
+					}
+					//アートワークが設定されていない場合
+					catch(IndexOutOfRangeException)
+					{
+						TempImageFilePaths.Add(new List<string> { listView1.Items[counter].SubItems[1].Text, null});
+                    }
+					counter++;
 				}
 			}
 		}
@@ -78,35 +107,33 @@ namespace mp3tageditor
 						//アーティストを取得
 						Artist_textbox.Text = artist;
 					}
+				}
 
-					MemoryStream ms;
-					try
+				//mp3ファイルから取得したアートワークをpictureboxに設定する
+				for(int i = 0; i < TempImageFilePaths.ToArray().GetLength(0); i++)
+				{
+					//今選択されている曲が一時的に取得している画像と一致していた場合
+					if(listView1.SelectedItems[0].SubItems[1].Text == TempImageFilePaths[i][0])
 					{
-						//アートワークを取得
-						TagLib.IPicture Artworks = mp3tags.Pictures[0];
-						ms = new MemoryStream(Artworks.Data.Data);
-						ms.Seek(0, SeekOrigin.Begin);
-						//画像をリサイズ
-						Bitmap canvas = new Bitmap(pictureBox1.Width, pictureBox1.Height);
-						Graphics g = Graphics.FromImage(canvas);
-						g.DrawImage(Image.FromStream(ms), 0, 0, pictureBox1.Width, pictureBox1.Height);
-						g.Dispose();
-						
-                        pictureBox1.Image = ImageResize(canvas, pictureBox1.Size);
-
-						Image backupimg;
-						using(Image imgsrc = Image.FromStream(ms))
-							backupimg = new Bitmap(imgsrc);
-						using(backupimg)
+						//アートワークが取得できたらpictureboxに設定する
+						if(TempImageFilePaths[i][1] != null)
 						{
-							//picturebox1をクリックした時の画像表示のために一時的にアートワークを保存
-							backupimg.Save(TempImageFilePath, ImageFormat.Png);
+							Image img = Image.FromFile(TempImageFilePaths[i][1]);
+							pictureBox1.Image = ImageResize(img, Consts.PICTUREBOX_THUMBNAIL_SIZE);
+							//BをKB(1024Byte = 1KB)で計算
+							Datasize_label.Text = (new FileInfo(TempImageFilePaths[i][1]).Length / 1024f).ToString("#,### KB");
+							Imagesize_label.Text = img.Width + "x" + img.Height;
+							break;
 						}
-					}
-					//アートワークが設定されていない場合
-					catch(IndexOutOfRangeException)
-					{
-						pictureBox1.Image = null;
+						//今選択されている曲が一時的に取得している画像と一致しているが
+						//曲自体に画像が設定されていない場合、pictureboxには何も設定しない
+						else if(TempImageFilePaths[i][1] == null)
+						{
+							pictureBox1.Image = null;
+							Datasize_label.Text = "NA";
+							Imagesize_label.Text = "NA";
+							break;
+						}
 					}
 				}
 			}
@@ -115,7 +142,15 @@ namespace mp3tageditor
 		private void pictureBox1_Click(object sender, EventArgs e)
 		{
 			if(listView1.SelectedItems.Count > 0)
-				ImageShow(null, Image.FromFile(TempImageFilePath));
+			{
+				for(int i = 0; i < TempImageFilePaths.ToArray().GetLength(0); i++)
+				{
+					//今選択されている曲が一時的に取得している画像と一致していたらその画像をアートワークとしてpictureboxに設定
+					if(TempImageFilePaths[listView1.SelectedItems[0].Index][1] != null &&
+						TempImageFilePaths[listView1.SelectedItems[0].Index][0] == TempImageFilePaths[i][0])
+						ImageShow(null, Image.FromFile(TempImageFilePaths[listView1.SelectedItems[0].Index][1]));
+				}
+			}
 		}
 
 		/// <summary>
@@ -124,32 +159,29 @@ namespace mp3tageditor
 		/// </summary>
 		/// <param name="url">画像が格納されたURLを指定。省略可。</param>
 		/// <param name="img">画像が格納されたImageを指定。省略可。</param>
-		private void ImageShow(string url = null, Image img = null)
+		private async void ImageShow(string url = null, Image img = null)
 		{
 			Image imgdata = null;
 
 			if(url != null)
 			{
-				HttpWebRequest hwreq = (HttpWebRequest)WebRequest.Create(new Uri(url));
-				using(HttpWebResponse hwres = (HttpWebResponse)hwreq.GetResponse())
-				{
-					//画像をダウンロード
-					Stream imgstream = hwres.GetResponseStream();
+				HttpClient hc = new HttpClient();
+				//Streamダウンロード
+				using(Stream imgstream = await hc.GetStreamAsync(url))
 					imgdata = Image.FromStream(imgstream);
-				}
 			}
 			else
 				imgdata = img;
 
 			//フォーム作成
 			Form imageshow = new Form();
-			Size windowsize = new Size(500, 500) - new Size(16, 38);
+			Size UnknownSize = new Size(16, 38);
+			Size windowsize = new Size(500, 500) - UnknownSize;
 			imageshow.StartPosition = FormStartPosition.Manual;
 			imageshow.Location = new Point(this.Left + this.Width, this.Top);
 			imageshow.BackColor = Color.Black;
 			imageshow.Text = "Artwork Viewer";
-			imageshow.Size = windowsize + new Size(16, 38);
-			imageshow.SizeChanged += Imageshow_SizeChanged;
+			imageshow.Size = windowsize + UnknownSize;
 
 			//画像表示のためのpictureboxを作成
 			PictureBox pb = new PictureBox();
@@ -163,14 +195,6 @@ namespace mp3tageditor
 			imageshow.Controls.Add(pb);
 			imageshow.Show(this);
 		}
-
-		private void Imageshow_SizeChanged(object sender, EventArgs e)
-		{
-			Form test = (Form)sender;
-            Console.WriteLine(test.Size);
-		}
-
-
 
 		/// <summary>
 		/// 画像をリサイズする。
@@ -190,14 +214,36 @@ namespace mp3tageditor
 
 		private void button1_Click(object sender, EventArgs e)
 		{
-			ImageShow("http://vignette3.wikia.nocookie.net/akuma-high-next-generation/images/f/fc/Nepgear_photo.jpg/revision/latest?cb=20130314231032");
+			//ImageShow("http://ecx.images-amazon.com/images/I/61EkLn43QvL.jpg");
+			for(int i = 0; i < TempImageFilePaths.ToArray().GetLength(0); i ++)
+			{
+				Console.WriteLine("["+i+"][0]" + TempImageFilePaths[i][0]);
+				Console.WriteLine("["+i+"][1]" + TempImageFilePaths[i][1] + "\n");
+			}
 		}
 
-		private static string tempimagefilepath = Path.GetTempFileName();
-		public static string TempImageFilePath
+		/// <summary>
+		/// 一次元：mp3ファイルのパス。二次元要素のIDとして使用する。 二次元：mp3から読み込んだアートワークをpictureboxに表示するための画像のパス
+		/// </summary>
+		private static List<List<string>> tempimagefilepaths = new List<List<string>>();
+		public static List<List<string>> TempImageFilePaths
 		{
-			get { return tempimagefilepath; }
+			get { return tempimagefilepaths; }
+		}
+		/// <summary>
+		/// インターネットからダウンロードをキャッシュとして使用する画像のパス
+		/// </summary>
+		private static List<string> cacheimagefilepaths = new List<string>();
+		public static List<string> CacheImageFilePaths
+		{
+			set { cacheimagefilepaths = value; }
+			get { return cacheimagefilepaths; }
 		}
 
 	}
+}
+
+class Consts
+{
+	public static readonly Size PICTUREBOX_THUMBNAIL_SIZE = new Size(160, 153);
 }
